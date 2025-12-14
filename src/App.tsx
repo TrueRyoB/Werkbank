bdc1212dcc96ff7420b3b2469d236d43f0c7dcd2
TrueRyoB <trueryob@proton.me>
2025-12-13 21:10:46 -0600

    fix: modified a footer UI for a better visuality

--- src/App.tsx
+++ src/App.tsx
@@ -9,7 +9,7 @@ function App() {
   return (
     <div className="flex flex-col w-full h-full items-center justify-start">
     {/* header */}
-    <div className="header flex flex-col gap-[5px] bg-green-900 text-white p-4 w-full">
+    <div className="header flex flex-col gap-[5px] bg-primary text-white p-4 w-full">
       <span className="text-2xl">Werkbank</span>
       <span>A browser text editor that generates bonsai based on your hashed text.</span>
     </div>
@@ -20,7 +20,7 @@ function App() {
         <div className="header flex flex-row justify-between">
           <span>Input Field</span>
           <div className="btn-container">
-            <span id="clear-input" className="bg-green-800 py-0.5 px-1 br-1 rounded text-white">clear</span>
+            <span id="clear-input" className="bg-primary-lighter py-0.5 px-1 br-1 rounded text-white">clear</span>
           </div>
         </div>
         <span className="block h-[8px]"></span>
@@ -40,8 +40,8 @@ function App() {
           <div className="header flex flex-row justify-between w-full p-2 gap-[4px]">
             <span>Bonsai art</span>
             <div className="flex flex-row gap-[4px] justify-center items-center">
-              <span id="copy-ascii" className="bg-green-800 py-0.5 px-1 br-1 rounded text-white">copy</span>
-              <span id="share-URL" className="bg-green-800 py-0.5 px-1 br-1 rounded text-white">share URL</span>
+              <span id="copy-ascii" className="bg-primary-lighter py-0.5 px-1 br-1 rounded text-white">copy</span>
+              <span id="share-URL" className="bg-primary-lighter py-0.5 px-1 br-1 rounded text-white">share URL</span>
             </div>
           </div>
           <AsciiContainer/>
@@ -49,8 +49,8 @@ function App() {
       </div>
     </div>
     {/* footer */}
-    <div className="footer w-full bg-gray-100 p-1">
-      <p className="text-gray-500">&copy; 2026 DeepNapEngine</p>
+    <div className="footer h-[16px] bg-white">
+      <p className="text-white">&copy; 2026 DeepNapEngine</p>
     </div>
     </div>
   );
